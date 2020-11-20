66262c99f50173f3bb4ca98430ed30d086e3c740
Michael Steinert <michael.steinert.94@gmail.com>
2020-11-20 21:10:19 +0100

    Added JSON Web Token

---
+++ src/main/java/com/example/springsecurityjwt/ResourceController.java
@@ -0,0 +1,49 @@
+package com.example.springsecurityjwt;
+
+import com.example.springsecurityjwt.models.AuthenticationRequest;
+import com.example.springsecurityjwt.models.AuthenticationResponse;
+import com.example.springsecurityjwt.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ResourceController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @GetMapping("/hello")
+    public String printHello() {
+        return "Hello World";
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+            );
+        } catch (BadCredentialsException e) {
+            throw new Exception("Incorrect Username or Password", e);
+        }
+
+        final UserDetails userDetails = myUserDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+        final String jwt = jwtUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    }
+
+}
